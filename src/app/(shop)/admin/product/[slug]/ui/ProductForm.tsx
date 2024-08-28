8a1f0bc37292e4f5437f0c18cff3b019657fb458
Alexis <alexiszanotti@gmail.com>
2024-08-28 20:28:32 +0200

    implement initial functionality

--- src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
@@ -2,11 +2,10 @@
 
 import { useForm } from "react-hook-form";
 import { Category, Product, ProductImage as ProductWithImage } from "@/interfaces";
-import Image from "next/image";
 import clsx from "clsx";
 import { createUpdateProduct, deleteProductImage } from "@/actions";
-import { useRouter } from 'next/navigation';
-import { ProductImage } from '@/components';
+import { useRouter } from "next/navigation";
+import { ProductImage } from "@/components";
 
 interface Props {
   product: Partial<Product> & { ProductImage?: ProductWithImage[] };
@@ -30,7 +29,6 @@ interface FormInputs {
 }
 
 export const ProductForm = ({ product, categories }: Props) => {
-
   const router = useRouter();
 
   const {
@@ -63,10 +61,10 @@ export const ProductForm = ({ product, categories }: Props) => {
 
     const { images, ...productToSave } = data;
 
-    if ( product.id ){
+    if (product.id) {
       formData.append("id", product.id ?? "");
     }
-    
+
     formData.append("title", productToSave.title);
     formData.append("slug", productToSave.slug);
     formData.append("description", productToSave.description);
@@ -76,101 +74,97 @@ export const ProductForm = ({ product, categories }: Props) => {
     formData.append("tags", productToSave.tags);
     formData.append("categoryId", productToSave.categoryId);
     formData.append("gender", productToSave.gender);
-    
-    if ( images ) {
-      for ( let i = 0; i < images.length; i++  ) {
-        formData.append('images', images[i]);
+
+    if (images) {
+      for (let i = 0; i < images.length; i++) {
+        formData.append("images", images[i]);
       }
     }
 
+    const { ok, product: updatedProduct } = await createUpdateProduct(formData);
 
-
-    const { ok, product:updatedProduct } = await createUpdateProduct(formData);
-
-    if ( !ok ) {
-      alert('Producto no se pudo actualizar');
+    if (!ok) {
+      alert("Producto no se pudo actualizar");
       return;
     }
 
-    router.replace(`/admin/product/${ updatedProduct?.slug }`)
-
-
+    router.replace(`/admin/product/${updatedProduct?.slug}`);
   };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3"
+      className='grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3'
     >
       {/* Textos */}
-      <div className="w-full">
-        <div className="flex flex-col mb-2">
+      <div className='w-full'>
+        <div className='flex flex-col mb-2'>
           <span>Título</span>
           <input
-            type="text"
-            className="p-2 border rounded-md bg-gray-200"
+            type='text'
+            className='p-2 border rounded-md bg-gray-200'
             {...register("title", { required: true })}
           />
         </div>
 
-        <div className="flex flex-col mb-2">
+        <div className='flex flex-col mb-2'>
           <span>Slug</span>
           <input
-            type="text"
-            className="p-2 border rounded-md bg-gray-200"
+            type='text'
+            className='p-2 border rounded-md bg-gray-200'
             {...register("slug", { required: true })}
           />
         </div>
 
-        <div className="flex flex-col mb-2">
+        <div className='flex flex-col mb-2'>
           <span>Descripción</span>
           <textarea
             rows={5}
-            className="p-2 border rounded-md bg-gray-200"
+            className='p-2 border rounded-md bg-gray-200'
             {...register("description", { required: true })}
           ></textarea>
         </div>
 
-        <div className="flex flex-col mb-2">
+        <div className='flex flex-col mb-2'>
           <span>Price</span>
           <input
-            type="number"
-            className="p-2 border rounded-md bg-gray-200"
+            type='number'
+            className='p-2 border rounded-md bg-gray-200'
             {...register("price", { required: true, min: 0 })}
           />
         </div>
 
-        <div className="flex flex-col mb-2">
+        <div className='flex flex-col mb-2'>
           <span>Tags</span>
           <input
-            type="text"
-            className="p-2 border rounded-md bg-gray-200"
+            type='text'
+            className='p-2 border rounded-md bg-gray-200'
             {...register("tags", { required: true })}
           />
         </div>
 
-        <div className="flex flex-col mb-2">
+        <div className='flex flex-col mb-2'>
           <span>Gender</span>
           <select
-            className="p-2 border rounded-md bg-gray-200"
+            className='p-2 border rounded-md bg-gray-200'
             {...register("gender", { required: true })}
           >
-            <option value="">[Seleccione]</option>
-            <option value="men">Men</option>
-            <option value="women">Women</option>
-            <option value="kid">Kid</option>
-            <option value="unisex">Unisex</option>
+            <option value=''>[Seleccione]</option>
+            <option value='men'>Men</option>
+            <option value='women'>Women</option>
+            <option value='kid'>Kid</option>
+            <option value='unisex'>Unisex</option>
           </select>
         </div>
 
-        <div className="flex flex-col mb-2">
+        <div className='flex flex-col mb-2'>
           <span>Categoria</span>
           <select
-            className="p-2 border rounded-md bg-gray-200"
+            className='p-2 border rounded-md bg-gray-200'
             {...register("categoryId", { required: true })}
           >
-            <option value="">[Seleccione]</option>
-            {categories.map((category) => (
+            <option value=''>[Seleccione]</option>
+            {categories.map(category => (
               <option key={category.id} value={category.id}>
                 {category.name}
               </option>
@@ -178,25 +172,25 @@ export const ProductForm = ({ product, categories }: Props) => {
           </select>
         </div>
 
-        <button className="btn-primary w-full">Guardar</button>
+        <button className='btn-primary w-full'>Guardar</button>
       </div>
 
       {/* Selector de tallas y fotos */}
-      <div className="w-full">
-        <div className="flex flex-col mb-2">
+      <div className='w-full'>
+        <div className='flex flex-col mb-2'>
           <span>Inventario</span>
           <input
-            type="number"
-            className="p-2 border rounded-md bg-gray-200"
+            type='number'
+            className='p-2 border rounded-md bg-gray-200'
             {...register("inStock", { required: true, min: 0 })}
           />
         </div>
 
         {/* As checkboxes */}
-        <div className="flex flex-col">
+        <div className='flex flex-col'>
           <span>Tallas</span>
-          <div className="flex flex-wrap">
-            {sizes.map((size) => (
+          <div className='flex flex-wrap'>
+            {sizes.map(size => (
               // bg-blue-500 text-white <--- si está seleccionado
               <div
                 key={size}
@@ -213,32 +207,32 @@ export const ProductForm = ({ product, categories }: Props) => {
             ))}
           </div>
 
-          <div className="flex flex-col mb-2">
+          <div className='flex flex-col mb-2'>
             <span>Fotos</span>
             <input
-              type="file"
-              { ...register('images') }
+              type='file'
+              {...register("images")}
               multiple
-              className="p-2 border rounded-md bg-gray-200"
-              accept="image/png, image/jpeg, image/avif"
+              className='p-2 border rounded-md bg-gray-200'
+              accept='image/png, image/jpeg, image/avif'
             />
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-            {product.ProductImage?.map((image) => (
+          <div className='grid grid-cols-1 sm:grid-cols-3 gap-3'>
+            {product.ProductImage?.map(image => (
               <div key={image.id}>
                 <ProductImage
                   alt={product.title ?? ""}
-                  src={ image.url }
+                  src={image.url}
                   width={300}
                   height={300}
-                  className="rounded-t shadow-md"
+                  className='rounded-t shadow-md'
                 />
 
                 <button
-                  type="button"
+                  type='button'
                   onClick={() => deleteProductImage(image.id, image.url)}
-                  className="btn-danger w-full rounded-b-xl"
+                  className='btn-danger w-full rounded-b-xl'
                 >
                   Eliminar
                 </button>
